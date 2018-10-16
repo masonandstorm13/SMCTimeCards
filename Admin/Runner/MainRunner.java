c0a0dd84d4b59286ec78c303196c8b708a029d24
masonandstorm13 <masonandstorm13@gmail.com>
2018-10-16 16:33:32 -0400

    added the abillity to select mutli part projects from new work order

--- Admin/Runner/MainRunner.java
+++ Admin/Runner/MainRunner.java
@@ -8,12 +8,13 @@ import Gui.EmployeeMain;
 import Gui.ExtraMain;
 import Gui.WorkInProgressMain;
 import Gui.WorkOrderMainGui;
+import Gui.WorkOrderMultiPartSelect;
 import Gui.newWorkOrder;
 
 public class MainRunner {
 
 	public static void main(String[] args) {
-		runAdminMainGui();
+		runWorkOrderMultiParSelect();
 	}
 	
 	public static void runAdminMainGui() {
@@ -153,6 +154,53 @@ public class MainRunner {
         		//if the frame does not exist already it is created
         		if(exist == false) {
         			new newWorkOrder().setVisible(true);
+        		}
+            }
+        });
+	}
+	
+	public static void runWorkOrderMultiParSelect() {
+		// TODO Auto-generated method stub
+		/* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(AdminMainGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(AdminMainGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(AdminMainGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(AdminMainGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+			public void run() {
+            	//boolean that checks if Frame is already in memory
+            	Boolean exist = false;
+            	
+            	//finds current frame out of listed frames and disposes of it
+        		for(int i = 0; i < Frame.getFrames().length; i++) {
+        			if(Frame.getFrames()[i].getTitle().equals("MultiPartSelect")) {
+        				Frame.getFrames()[i].setVisible(true);
+        				exist = true;
+        			}
+        		}
+        		
+        		//if the frame does not exist already it is created
+        		if(exist == false) {
+                    new WorkOrderMultiPartSelect().setVisible(true);
         		}
             }
         });
